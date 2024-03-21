a862087054b4259d2f6fc6a6be4b59f15339bece
Marty Kandes <mkandes@sdsc.edu>
2024-03-20 19:40:28 -0700

    Create build-pi-omp.sh

---
+++ build-pi-omp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=build-pi-omp
+#SBATCH --account=use300
+#SBATCH --partition=debug
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=00:02:00
+#SBATCH --output=%x.o%j.%N
+
+module reset
+module load gcc/10.2.0
+module list
+printenv
+
+make
