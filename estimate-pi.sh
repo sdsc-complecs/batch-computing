969dd847bcd1e793b3b2d1f781688ecc0f551873
Marty Kandes <mkandes@sdsc.edu>
2024-10-17 08:58:05 -0700

    Create estimate-pi.sh

---
+++ estimate-pi.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=estimate-pi
+#SBATCH --account=use300
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=00:30:00
+#SBATCH --output=%x.o%j.%N
+
+module purge
+
+time -p "${HOME}/4pi/bash/pi.sh" -b 8 -r 5 -s 10000
