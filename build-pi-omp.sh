306bea0d96b99068310bdc546ec3445e4e5654ab
Marty Kandes <mkandes@sdsc.edu>
2025-08-21 12:40:28 -0700

    Update build-pi-omp.sh

--- build-pi-omp.sh
+++ build-pi-omp.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name=build-pi-omp
-#SBATCH --account=gue998
-#SBATCH --reservation=ciml25cpu
 #SBATCH --partition=shared
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
